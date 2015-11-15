3f48868497bf41694cdba5349ff9509698dac26d
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-15 14:33:20 -0500

    Convert to ex6.

--- catch-of-the-day/scripts/components/StorePicker.js
+++ catch-of-the-day/scripts/components/StorePicker.js
@@ -4,18 +4,22 @@
 */
 
 import React from 'react';
-import { Navigation } from 'react-router';
+import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
-var StorePicker = React.createClass({
-  mixins : [Navigation],
-  goToStore : function(event) {
+@autobind
+class StorePicker extends React.Component {
+  
+  goToStore(event) {
     event.preventDefault();
     // get the data from the input
+    console.log(this);
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render : function() {
+  }
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -23,8 +27,9 @@ var StorePicker = React.createClass({
         <input type="Submit" />
       </form>
     )
-  }
+  }  
+}
 
-});
+reactMixin.onClass(StorePicker, History);
 
 export default StorePicker;

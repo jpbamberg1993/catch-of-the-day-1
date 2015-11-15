e490034d78caed1afa9c8d3d531d042b7bf409b8
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-15 13:05:04 -0500

    Moved Order into its own class.

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -26,6 +26,7 @@ import StorePicker from './components/StorePicker';
 import Fish from './components/Fish';
 import AddFishForm from './components/AddFishForm';
 import Header from './components/Header';
+import Order from './components/Order';
 
 
 /*
@@ -107,76 +108,6 @@ var App = React.createClass({
 		)
 	}
 });
-
-
-/*
-	Order
-*/
-
-var Order = React.createClass({
-	renderOrder : function(key) {
-		var fish = this.props.fishes[key];
-		var count = this.props.order[key];
-		var removeButton = <button onClick={this.props.removeFromOrder.bind(null,key)}>&times;</button>
-
-		if (!fish) {
-			return <li key={key}>Sorry, fish no longer available!{removeButton}</li>
-		}
-
-		return (
-			<li key={key}>
-				<span>
-					<CSSTransitionGroup component="span"
-															transitionName="count"
-															transitionLeaveTimeout={250}
-															transitionEnterTimeout={250}>
-						<span key={count}>{count}</span>
-					</CSSTransitionGroup>
-
-					lbs {fish.name} {removeButton}
-					<span className="price">{h.formatPrice(count * fish.price)}</span>
-				</span>
-			</li>)
-	},
-	render : function() {
-		var orderIds = Object.keys(this.props.order);
-		var total = orderIds.reduce((prevTotal, key)=> {
-			var fish = this.props.fishes[key];
-			var count = this.props.order[key];
-			var isAvailable = fish && fish.status === 'available';
-
-			if(fish && isAvailable) {
-				return prevTotal + (count * parseInt(fish.price) || 0);
-			}
-
-			return prevTotal;
-		}, 0);
-		
-		return (
-			<div className="order-wrap">
-				<h2 className="order-title">Your Order</h2>
-				
-				<CSSTransitionGroup className="order"
-														component="ul"
-														transitionName="order"
-														transitionEnterTimeout={500}
-														transitionLeaveTimeout={500}>
-					{orderIds.map(this.renderOrder)}
-					<li className="total">
-						<strong>Total:</strong>
-						{h.formatPrice(total)}
-					</li>
-				</CSSTransitionGroup>
-			
-			</div>
-		)
-	},
-	propTypes : {
-		fishes : React.PropTypes.object.isRequired,
-		order : React.PropTypes.object.isRequired,
-		removeFromOrder : React.PropTypes.func.isRequired
-	}
-})
 
 
 /*

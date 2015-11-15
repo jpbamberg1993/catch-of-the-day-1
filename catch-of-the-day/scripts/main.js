51bdefdb027ddd3b7d235cf2708c55f657fd997c
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-15 13:26:42 -0500

    Moved Inventory component to class.

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -27,6 +27,7 @@ import Fish from './components/Fish';
 import AddFishForm from './components/AddFishForm';
 import Header from './components/Header';
 import Order from './components/Order';
+import Inventory from './components/Inventory';
 
 
 /*
@@ -108,50 +109,6 @@ var App = React.createClass({
 		)
 	}
 });
-
-
-/*
-	Inventory  
-*/
-
-var Inventory = React.createClass({
-	renderInventory : function(key) {
-		var linkState = this.props.linkState;
-		return (
-			<div className="fish-edit" key={key}>
-				<input type="text" valueLink={linkState('fishes.' + key + '.name')}/>
-				<input type="text" valueLink={linkState('fishes.' + key + '.price')}/>
-				<select valueLink={linkState('fishes.' + key + '.status')}>
-					<option value="unavailable">Sold Out!</option>
-					<option value="available">Fresh!</option>
-				</select>
-
-				<textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
-				<input type="text" valueLink={linkState('fishes.' + key + '.image')} />
-				<button onClick={this.props.removeFish.bind(null, key)}>Remove Fish!</button>
-			</div>
-		)
-	},
-	render : function() {
-		return (
-			<div>
-				<h2>Inventory</h2>
-
-				{Object.keys(this.props.fishes).map(this.renderInventory)}
-
-				<AddFishForm {...this.props} />
-				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
-			</div>
-		)
-	},
-	propTypes : {
-		addFish : React.PropTypes.func.isRequired,
-		loadSamples : React.PropTypes.func.isRequired,
-		fishes : React.PropTypes.object.isRequired,
-		linkState : React.PropTypes.func.isRequired,
-		removeFish : React.PropTypes.func.isRequired
-	}
-})
 
 
 /*

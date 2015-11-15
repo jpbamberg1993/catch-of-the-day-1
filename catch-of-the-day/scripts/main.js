c1e0b4373f7d6246a9ae3a6176d903f465e4d89c
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-15 13:00:28 -0500

    Moved Header into class.

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -25,6 +25,7 @@ import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 import Fish from './components/Fish';
 import AddFishForm from './components/AddFishForm';
+import Header from './components/Header';
 
 
 /*
@@ -106,30 +107,6 @@ var App = React.createClass({
 		)
 	}
 });
-
-
-/*
-	Header
-*/
-
-var Header = React.createClass({
-	render : function() {
-		return (
-			<header className="top">
-				<h1>Catch
-					<span className="ofThe">
-						<span className="of">of</span>
-						<span className="the">the</span>
-					</span>
-					Day</h1>
-				<h3 className="tagline"><span>{this.props.tagline}</span></h3>
-			</header>
-		)
-	},
-	propTypes : {
-		tagline : React.PropTypes.string.isRequired
-	}
-})
 
 
 /*
